2b22deb48e5c7583e15739e06331c291e8745aed
Sonny <3076270s@gmail.com>
2022-02-03 11:32:03 +0900

    modified port num

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ var indexRouter = require("./routes/index");
 const klaytnRouter = require("./routes/klaytnRouter");
 
 var app = express();
-const port = 3001;
+const port = 80;
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
